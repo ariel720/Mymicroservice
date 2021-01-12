b5ffaef4cd3578e5b49e594961e970d652c05f00
Shinhee Kim <Shinhee.Kim@GeorgianCollege.ca>
2021-01-11 22:08:49 -0500

    related table created

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into user values (1, sysdate(), 'Jin');
 insert into user values (2, sysdate(), 'Shinhee');
 insert into user values (3, sysdate(), 'Ryna');
+
+insert into post values (1, 'my first post', 1);
+insert into post values (2, 'my first post', 1);
